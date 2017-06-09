7cadcd4b97f4f0fa19442de9c6e075c3640d4bb6
Christopher Pitt <c.james.pitt@gmail.com>
2017-06-09 12:18:41 +0100

    Add a back link to the homepage from the blog

--- pages/blog/_template.jsx
+++ pages/blog/_template.jsx
@@ -1,9 +1,16 @@
-import { PropTypes } from 'react'
+import React, { PropTypes } from 'react'
+import { Link } from 'react-router'
+import { prefixLink } from 'gatsby-helpers'
 import Blog from '../../src/components/Blog'
 
 const Template = ({ children }) => {
   if (children && children.props && children.props.route && children.props.route.page) {
-    return Blog(children.props.route.page);
+    return (
+      <div>
+        <Link className="back-link" to="/"> Back </Link>
+        {Blog(children.props.route.page)}
+      </div>
+    )
   }
   return null;
 }
