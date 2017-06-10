b2654df4fd86eb8bba0ce7ddb53e30a1c6e1207a
Christopher Pitt <c.james.pitt@gmail.com>
2017-06-10 09:36:35 +0100

    Add basic gallery display

--- pages/gallery/_template.jsx
+++ pages/gallery/_template.jsx
@@ -1,14 +1,21 @@
 import React, { PropTypes } from 'react'
+import { Link } from 'react-router'
+import { prefixLink } from 'gatsby-helpers'
 import Gallery from '../../src/components/Gallery'
 
 const Template = ({ children }) => {
   if (children && children.props && children.props.route && children.props.route.page) {
-    // return gallery index page
+    // return index page as is
     if (children.props.route.page.file.name === 'index') {
       return <div>{children}</div>
     }
-    // return gallery component
-    return <Gallery data={children.props.route.page.data}/>
+    // otherwise return the gallery
+    return (
+      <div>
+        <Link className="back-link" to={prefixLink('/gallery/')}> Back </Link>
+        {Gallery(children.props.route.page)}
+      </div>
+    )
   }
   return null
 }
--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,28 +1,25 @@
 import React from 'react'
 
-const renderImage = (data) => {
-  const { imageTitle, imageDescr, image } = data;
-  return (
-    <div key={imageTitle}>
-      <h2>{imageTitle}</h2>
-      <img src={image} />
-      <p>{imageDescr}</p>
-    </div>
-  )
-}
+const GalleryImage = img =>
+  <div className="gallery-item">
+    <h6 className="gallery-item-title">{img.imageTitle}</h6>
+    <div className="gallery-item-image" style={{ backgroundImage: `url('${img.image}')` }} />
+    <p>{img.imageDescr}</p>
+  </div>
 
-
-const Gallery = ({ data }) => {
-  const { title, images } = data;
-  return (
-    <div>
-      <h1>{title}</h1>
-      {images.map(image => renderImage(image))}
+const Gallery = page =>
+  <div key={page.path} className="gallery">
+    <h2 className="gallery-title">{page.data.title}</h2>
+    <div className="gallery-content" dangerouslySetInnerHTML={{ __html: page.data.body }} />
+    <div className="gallery-images">
+      {page.data.images.map(img =>
+        <GalleryImage key={img.image} {...img} />
+      )}
     </div>
-  )
-};
+  </div>
 
 Gallery.propTypes = {
+
 }
 
 export default Gallery
