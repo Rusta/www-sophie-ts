39640373c80e9b62cdb42fda88fed7a4dcfafb76
Christopher Pitt <c.james.pitt@gmail.com>
2017-06-12 10:04:32 +0100

    only show metadata if we have a valid date, and therefore move the border styling onto the entry-body wrapper

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -7,20 +7,23 @@ const Blog = (page) => {
   const date = moment(page.data.date, dateFormat);
   const dateFriendly = date.format('dddd Do MMMM YYYY (HH:mm a)');
   const daysAgo = date.fromNow();
-
   return (
     <div key={page.path} className="entry">
       <h2 className="entry-title">{page.data.title}</h2>
-      <div className="entry-meta">
-        <span className="entry-date">{dateFriendly}</span>
-        <span className="entry-ago">{daysAgo}</span>
-      </div>
-      {page.data.image && (
-        <div className="entry-image">
-          <img alt={page.data.title} src={page.data.image} />
+      {date.isValid() && (
+        <div className="entry-meta">
+          <span className="entry-date">{dateFriendly}</span>
+          <span className="entry-ago">{daysAgo}</span>
         </div>
       )}
-      <div className="entry-content" dangerouslySetInnerHTML={{ __html: page.data.body }} />
+      <div className="entry-body">
+        {page.data.image && (
+          <div className="entry-image">
+            <img alt={page.data.title} src={page.data.image} />
+          </div>
+        )}
+        <div className="entry-content" dangerouslySetInnerHTML={{ __html: page.data.body }} />
+      </div>
     </div>
   );
 };
