d4574a192c48774cead8a507d425d856754d686e
Christopher Pitt <c.james.pitt@gmail.com>
2017-06-15 17:14:10 +0100

    add social links to the footer, and attribution for the icons

--- src/components/SiteFooter/index.js
+++ src/components/SiteFooter/index.js
@@ -1,16 +1,31 @@
 import React from 'react'
+import IconTwitter from './iconTwitter'
+import IconFacebook from './iconFacebook'
+import IconInstagram from './iconInstagram'
+import IconTripadvisor from './iconTripadvisor'
+
+const svgProps = {
+  width: '26px',
+  height: '26px'
+};
 
 const SiteFooter = () =>
   <footer>
-    <div>
-      <a href="https://www.facebook.com/SophieTsVintageTeashop/" target="_blank">Facebook</a>
-      |
-      <a href="https://www.tripadvisor.co.uk/Restaurant_Review-g187045-d2278613-Reviews-Sophie_T_s_Vintage_Teashop-Peterborough_Cambridgeshire_England.html" target="_blank">TripAdvisor</a>
-      |
-      <a href="https://twitter.com/sophietsteashop" target="_blank">Twitter @sophietsteashop</a>
-      |
-      <a href="https://www.instagram.com/explore/locations/400433675/sophie-ts-vintage-teashop/" target="_blank">Instagram</a>
+    <div className="social">
+      <a href="https://www.facebook.com/SophieTsVintageTeashop/" target="_blank">
+        <IconFacebook {...svgProps} />
+      </a>
+      <a href="https://www.tripadvisor.co.uk/Restaurant_Review-g187045-d2278613-Reviews-Sophie_T_s_Vintage_Teashop-Peterborough_Cambridgeshire_England.html" target="_blank">
+        <IconTripadvisor {...svgProps} />
+      </a>
+      <a href="https://twitter.com/sophietsteashop" target="_blank">
+        <IconTwitter {...svgProps} />
+      </a>
+      <a href="https://www.instagram.com/explore/locations/400433675/sophie-ts-vintage-teashop/" target="_blank">
+        <IconInstagram {...svgProps} />
+      </a>
     </div>
+    <small className="attribution"> Icons by FlatIcon </small>
   </footer>
 
 export default SiteFooter
